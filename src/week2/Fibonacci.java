cbdcd4f83a9f8cedad9737acb3e405771b550396
HCVincent <lilacsweet@163.com>
2022-08-27 00:02:24 +0800

    solve index problem

--- src/week2/Fibonacci.java
+++ src/week2/Fibonacci.java
@@ -3,27 +3,25 @@ package week2;
 import java.util.Scanner;
 
 public class Fibonacci {
-//	private static long calc_fib(int n) {
-//		if (n <= 1)
-//			return n;
-//
-//		return calc_fib(n - 1) + calc_fib(n - 2);
-//	}
 
 	private static long calc_fib(int n) {
-		int[] li = new int[n];
-		li[0] = 1;
-		li[1] = 1;
-		for(int i = 2; i < n; i++) {
-			li[i] = li[i - 1] + li[i - 2];
+		if (n <= 1) {
+			return n;
+		} else {
+			int[] li = new int[n + 1];
+			li[0] = 0;
+			li[1] = 1;
+			for (int i = 2; i < n + 1; i++) {
+				li[i] = li[i - 1] + li[i - 2];
+			}
+			return li[n];
 		}
-		return li[n-1];
+
 	}
 
 	public static void main(String args[]) {
 		Scanner in = new Scanner(System.in);
 		int n = in.nextInt();
-		in.close();
 		System.out.println(calc_fib(n));
 	}
 }
