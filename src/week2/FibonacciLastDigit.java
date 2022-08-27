029bb7c46c14af5b2e9d5c202cc4ab5be49a79e0
HCVincent <lilacsweet@163.com>
2022-08-27 16:03:08 +0800

    solve with mod earlier, save time

--- src/week2/FibonacciLastDigit.java
+++ src/week2/FibonacciLastDigit.java
@@ -1,31 +1,24 @@
 package week2;
 
 import java.util.*;
-import java.math.BigInteger;
-import java.util.concurrent.TimeUnit;
 
 public class FibonacciLastDigit {
     private static int getFibonacciLastDigitNaive(int n) {
         if (n <= 1)
             return n;
 
-        BigInteger previous = BigInteger.ZERO;
-        BigInteger current  = BigInteger.ONE;
+        int previous = 0;
+        int current  = 1;
         int result = 0;
 
         for (int i = 0; i < n - 1; ++i) {
-        	BigInteger tmp_previous = previous;
-            previous = current;
-            long startTime = System.nanoTime();
-            current = tmp_previous.add(current);
-            result = current.mod(BigInteger.TEN).intValue();
-            long endTime = System.nanoTime();
-            long timeElapsed = endTime - startTime;
-            System.out.println("Execution time in milliseconds: " + timeElapsed);
+            int tmp_previous = previous;
+            previous = current % 10;
+            current = tmp_previous + current % 10;
             
         }
 
-        return result;
+        return current % 10;
     }
     
     public static void main(String[] args) {
