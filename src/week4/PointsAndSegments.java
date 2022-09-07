38297f196e3e2685544d39a1ef60777190b95591
HCVincent <lilacsweet@163.com>
2022-09-07 15:24:33 +0800

    Organizing a Lottery Problem solved

---
+++ src/week4/PointsAndSegments.java
@@ -0,0 +1,68 @@
+package week4;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PointsAndSegments {
+
+    private static int[] fastCountSegments(int[] starts, int[] ends, int[] points) {
+        int[] cnt = new int[points.length];
+        //write your code here
+
+        Arrays.sort(starts);
+        Arrays.sort(ends);
+
+        for (int i = 0; i < points.length; i++) {
+            int j, k;
+            for (j = 0; j < starts.length; j++) {
+                if (starts[j] > points[i]) {
+                    break;
+                }
+            }
+
+            for (k = 0; k < ends.length; k++) {
+                if (ends[k] >= points[i]) {
+                    break;
+                }
+            }
+            cnt[i] = j - k;
+        }
+
+        return cnt;
+    }
+
+    private static int[] naiveCountSegments(int[] starts, int[] ends, int[] points) {
+        int[] cnt = new int[points.length];
+        for (int i = 0; i < points.length; i++) {
+            for (int j = 0; j < starts.length; j++) {
+                if (starts[j] <= points[i] && points[i] <= ends[j]) {
+                    cnt[i]++;
+                }
+            }
+        }
+        return cnt;
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n, m;
+        n = scanner.nextInt();
+        m = scanner.nextInt();
+        int[] starts = new int[n];
+        int[] ends = new int[n];
+        int[] points = new int[m];
+        for (int i = 0; i < n; i++) {
+            starts[i] = scanner.nextInt();
+            ends[i] = scanner.nextInt();
+        }
+        for (int i = 0; i < m; i++) {
+            points[i] = scanner.nextInt();
+        }
+        //use fastCountSegments
+        int[] cnt = fastCountSegments(starts, ends, points);
+        for (int x : cnt) {
+            System.out.print(x + " ");
+        }
+    }
+}
+
