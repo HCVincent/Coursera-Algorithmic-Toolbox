7583e9bfbb78e0c86de4e4053b8de71a22f59f07
HCVincent <lilacsweet@163.com>
2022-09-08 22:43:13 +0800

    Change Money DP solved

--- src/week5/ChangeDP.java
+++ src/week5/ChangeDP.java
@@ -1,30 +1,65 @@
 package week5;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class ChangeDP {
 	private static int getChange(int m, int[] coins) {
 		// write your code here
-		if (m == 0)
-			return 0;
-		int res = Integer.MAX_VALUE;
-
-		for (int i = 0; i < coins.length; i++) {
-			if (coins[i] <= m) {
-				int sub_res = getChange(m-coins[i], coins);
-				if (sub_res != Integer.MAX_VALUE && sub_res + 1 < res) {
-					res = sub_res + 1;
+		int results[] = new int[m + 1];
+		results[0] = 0;
+		for (int i = 1; i <= m; i++) {
+			results[i] = Integer.MAX_VALUE;
+		}
+		for (int i = 1; i <= m; i++) {
+			for (int j = 0; j < coins.length; j++) {
+				if(coins[j] <= i) {
+					int sub_res = results[i - coins[j]];
+					if(sub_res != Integer.MAX_VALUE && sub_res + 1 < results[i]) {
+						results[i] = sub_res + 1;
+					}
 				}
 			}
 		}
-		return res;
+		return results[m];
 	}
 
 	public static void main(String[] args) {
-		Scanner scanner = new Scanner(System.in);
+		FastScanner scanner = new FastScanner(System.in);
 		int m = scanner.nextInt();
 		int[] coins = { 4, 3, 1 };
 		System.out.println(getChange(m, coins));
 
+	}
+
+	static class FastScanner {
+		BufferedReader br;
+		StringTokenizer st;
+
+		FastScanner(InputStream stream) {
+			try {
+				br = new BufferedReader(new InputStreamReader(stream));
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+
+		String next() {
+			while (st == null || !st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
 	}
 }
