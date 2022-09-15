02eecfd91d0d3f688e80c241a435d4d168636580
HCVincent <lilacsweet@163.com>
2022-09-15 19:41:43 +0800

    Closest Points problem failed case

--- src/week4/Closest.java
+++ src/week4/Closest.java
@@ -23,49 +23,76 @@ public class Closest {
 		}
 	}
 
-	static double minimalDistance(int[] x, int y[]) {
-		double ans = Double.POSITIVE_INFINITY;
-		// write your code here
+	// A structure to represent a Point in 2D plane
+	public static class PointXComparator implements Comparator<Point> {
 
-		return ans;
+		// Needed to sort array of points
+		// according to X coordinate
+		@Override
+		public int compare(Point pointA, Point pointB) {
+			return Long.compare(pointA.x, pointB.x);
+		}
+
 	}
 
-	static double minimalDistanceSquare(Point[] pList, int startIndex, int endIndex) {
-		double ans = Double.POSITIVE_INFINITY;
-		// write your code here
-		if (startIndex + 1 == endIndex)
-			return Math.pow(pList[0].x - pList[1].x, 2) + Math.pow(pList[0].y - pList[1].y, 2);
+	public static class PointYComparator implements Comparator<Point> {
+
+		// Needed to sort array of points
+		// according to Y coordinate
+		@Override
+		public int compare(Point pointA, Point pointB) {
+			return Long.compare(pointA.y, pointB.y);
+		}
+
+	}
+
+	public static float closestUtil(Point[] P, int startIndex, int endIndex) {
+
+		// If there are 2 or 3 points, then use brute force
+		if ((endIndex - startIndex) <= 3) {
+			return bruteForce(P, endIndex);
+		}
+
+		// Find the middle point
 		int mid = startIndex + (endIndex - startIndex) / 2;
-		double dl = minimalDistanceSquare(pList, startIndex, mid);
-		double dr = minimalDistanceSquare(pList, mid, endIndex);
-		
-		double d = Math.min(dl, dr);
-		Point midPoint = pList[mid];
-		Point[] pListTemp = new Point[endIndex];
-		int tempSize = 0;
+		Point midPoint = P[mid];
+
+		// Consider the vertical line passing
+		// through the middle point calculate
+		// the smallest distance dl on left
+		// of middle point and dr on right side
+		float dl = closestUtil(P, startIndex, mid);
+		float dr = closestUtil(P, mid, endIndex);
+
+		// Find the smaller of two distances
+		float d = Math.min(dl, dr);
+
+		// Build an array strip[] that contains
+		// points close (closer than d)
+		// to the line passing through the middle point
+		Point[] strip = new Point[endIndex];
+		int j = 0;
 		for (int i = 0; i < endIndex; i++) {
-			if (Math.abs(pList[i].x - midPoint.x) < Math.sqrt(d)) {
-				pListTemp[tempSize] = pList[i];
-				tempSize++;
+			if (Math.abs(P[i].x - midPoint.x) < d) {
+				strip[j] = P[i];
+				j++;
 			}
 		}
-		ans = d;
-		int size = tempSize;
-		Arrays.sort(pListTemp, 0, tempSize, Comparator.comparing(p -> p.y));
-		for (int i = 0; i < size; i++) {
-			for (int j = i + 1; j < size && (pListTemp[j].y - pListTemp[i].y < Math.sqrt(ans)); ++j) {
-				if (Math.pow(pListTemp[i].x - pListTemp[j].x, 2)
-						+ Math.pow(pListTemp[i].y - pListTemp[j].y, 2) < ans) {
-					ans = Math.pow(pListTemp[i].x - pListTemp[j].x, 2)
-							+ Math.pow(pListTemp[i].y - pListTemp[j].y, 2);
-				}
-			}
-		}
-		return Math.min(ans, d);
+
+		// Find the closest points in strip.
+		// Return the minimum of d and closest
+		// distance is strip[]
+		return Math.min(d, stripClosest(strip, j, d));
 	}
-	
-	public static double minimalDistance(double powDistance) {
-		return Math.sqrt(powDistance);
+
+	// The main function that finds the smallest distance
+	// This method mainly uses closestUtil()
+	public static float closest(Point[] P, int n) {
+		Arrays.sort(P, 0, n, new PointXComparator());
+
+		// Use recursive function closestUtil()
+		// to find the smallest distance
+		return closestUtil(P, 0, n);
 	}
 
 	public static void main(String[] args) throws Exception {
@@ -74,14 +101,14 @@ public class Closest {
 		int n = nextInt();
 		int[] x = new int[n];
 		int[] y = new int[n];
-		Point[] pList = new Point[n];
+		Point[] points = new Point[n];
 		for (int i = 0; i < n; i++) {
 			x[i] = nextInt();
 			y[i] = nextInt();
 			Point p = new Point(x[i], y[i]);
-			pList[i] = p;
+			points[i] = p;
 		}
-		System.out.println(minimalDistance(minimalDistanceSquare(pList, 0, pList.length)));
+		System.out.println(closest(points, points.length));
 		writer.close();
 	}
 
@@ -107,4 +134,57 @@ public class Closest {
 	static int nextInt() {
 		return Integer.parseInt(next());
 	}
+
+	// A Brute Force method to return the
+	// smallest distance between two points
+	// in P[] of size n
+	public static float bruteForce(Point[] P, int n) {
+		float min = Float.MAX_VALUE;
+		float currMin = 0;
+
+		for (int i = 0; i < n; ++i) {
+			for (int j = i + 1; j < n; ++j) {
+				currMin = dist(P[i], P[j]);
+				if (currMin < min) {
+					min = currMin;
+				}
+			}
+		}
+		return min;
+	}
+
+	// A utility function to find the
+	// distance between two points
+	public static float dist(Point p1, Point p2) {
+		return (float) Math.sqrt((p1.x - p2.x) * (p1.x - p2.x) + (p1.y - p2.y) * (p1.y - p2.y));
+	}
+
+	// A utility function to find the
+	// distance between the closest points of
+	// strip of given size. All points in
+	// strip[] are sorted according to
+	// y coordinate. They all have an upper
+	// bound on minimum distance as d.
+	// Note that this method seems to be
+	// a O(n^2) method, but it's a O(n)
+	// method as the inner loop runs at most 6 times
+	public static float stripClosest(Point[] strip, int size, float d) {
+		float min = d; // Initialize the minimum distance as d
+
+		Arrays.sort(strip, 0, size, new PointYComparator());
+
+		// Pick all points one by one and try the next points till the difference
+		// between y coordinates is smaller than d.
+		// This is a proven fact that this loop runs at most 6 times
+		for (int i = 0; i < size; ++i) {
+			for (int j = i + 1; j < size && (strip[j].y - strip[i].y) < min; ++j) {
+				if (dist(strip[i], strip[j]) < min) {
+					min = dist(strip[i], strip[j]);
+				}
+			}
+		}
+
+		return min;
+	}
+
 }
